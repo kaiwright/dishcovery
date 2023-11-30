3ad1ddb7ce73898942b97a37ca1069f88f6d95d3
RlucSal <rluc.sal@gmail.com>
2023-11-30 22:07:51 +0000

    resolved merge conflicts

--- assets/script.js
+++ assets/script.js
@@ -2,29 +2,27 @@
 var EaternityURL = "https://co2.eaternity.ch/api/recipes/id?indicators=true"
 
 var ingredient = "eggplant";
-<<<<<<< HEAD
 console.log(queryURL)
 console.log(ingredient)
 
 
 
-var apiUrl = 'https://foodprint.p.rapidapi.com/api/foodprint/footprint/0.9';
-var apiKey = '6e625eaa59mshe756c05db8e818ap1218e4jsn938c80f4c954';
+// var apiUrl = 'https://foodprint.p.rapidapi.com/api/foodprint/footprint/0.9';
+// var apiKey = '6e625eaa59mshe756c05db8e818ap1218e4jsn938c80f4c954';
 
-fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': 'foodprint.p.rapidapi.com'
-    }
-})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-=======
+// fetch(apiUrl, {
+//     method: 'GET',
+//     headers: {
+//         'X-RapidAPI-Key': apiKey,
+//         'X-RapidAPI-Host': 'foodprint.p.rapidapi.com'
+//     }
+// })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
 
 var queryURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" + ingredient + "&app_id=2e4c3f7b&app_key=9937b42f06b25b17a8890d5216e8728a";
 
@@ -58,15 +56,7 @@ fetch(queryURL)
             console.log(cardTitle)
         }
 
->>>>>>> 4e9176d360e15dfb408f0adf924baacff134fd57
     });
 
 
 
-<<<<<<< HEAD
-    
-
-    
-    
-=======
->>>>>>> 4e9176d360e15dfb408f0adf924baacff134fd57
