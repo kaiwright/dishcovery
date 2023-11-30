8effc38eb09058014c54f8de47468e8d6dea1ce0
kaiwright <kaimatthewwright@gmail.com>
2023-11-30 14:15:48 +0000

    Got the API working

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var queryURL = "https://api.edamam.com/api/recipes/v2?type=public&q=" + ingredient + "&app_id=2e4c3f7b&app_key=9937b42f06b25b17a8890d5216e8728a";
+var EaternityURL = "https://co2.eaternity.ch/api/recipes/id?indicators=true"
+
+var ingredient = "eggplant";
+console.log(queryURL)
+console.log(ingredient)
