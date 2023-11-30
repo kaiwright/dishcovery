81979c7998d46f6e90ba1aaea30eb19d36c9aa39
RlucSal <rluc.sal@gmail.com>
2023-11-30 21:20:58 +0000

    added logic in carbon.js and removed from script.js

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,30 @@ var EaternityURL = "https://co2.eaternity.ch/api/recipes/id?indicators=true"
 var ingredient = "eggplant";
 console.log(queryURL)
 console.log(ingredient)
+
+
+
+var apiUrl = 'https://foodprint.p.rapidapi.com/api/foodprint/footprint/0.9';
+var apiKey = '6e625eaa59mshe756c05db8e818ap1218e4jsn938c80f4c954';
+
+fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'foodprint.p.rapidapi.com'
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+
+    
+
+    
+    
---
+++ carbon.js
@@ -0,0 +1,18 @@
+
+var apiUrl = 'https://foodprint.p.rapidapi.com/api/foodprint/footprint/0.9';
+var apiKey = '6e625eaa59mshe756c05db8e818ap1218e4jsn938c80f4c954';
+
+fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'foodprint.p.rapidapi.com'
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
